60ce4ce4835341a36dbcf2aa887813e884a514cb
inkyung <kyxxin@hanmail.net>
2020-10-21 19:20:17 +0900

    Refactor : hamburger menu button 라이브러리로 교체

--- src/components/Layout/header.tsx
+++ src/components/Layout/header.tsx
@@ -2,7 +2,7 @@ import React, {useState} from "react"
 import { Link } from "gatsby"
 import Logo from "../../../static/images/logo.svg";
 import { SiteMetadata } from "../../interfaces/SiteMetadata"
-
+import HamburgerMenu from 'react-hamburger-menu';
 
 interface Props {
   data : SiteMetadata,
@@ -22,15 +22,21 @@ return (
           </Link>
       </div>
 
-      <nav className="menu-wrap">
-        <div className={"menu-toggle-wrap " + (showMenu ? 'open' : '')} onClick={() => {setShowMenu(!showMenu)}}>
-          <div className="menu-toggle">
-            <div className="menu-ico">
-              <span/>
-            </div>
-          </div>
-        </div> 
+      <div className='hamburger-btn'>
+          <HamburgerMenu
+            isOpen={showMenu}
+            menuClicked={() => setShowMenu(!showMenu)}
+            width={28}
+            height={16}
+            strokeWidth={3}
+            rotate={0}
+            color={showMenu ? '#fff' : '#000'}
+            borderRadius={6}
+            animationDuration={0.5}
+          />
+      </div>
 
+      <nav className="menu-wrap">
         <div className={"menu-list-wrap " + (showMenu ? 'open' : '')}>
           <div className="menu-list">
           {data.menuLinks.map(link => (
